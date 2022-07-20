5c2bb24c2a995742d9729877bc67034afa3d042b
Peter Jang <peter.x.jang@gmail.com>
2022-07-20 16:03:20 -0500

    Add photos index action

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import PhotosIndex from "./PhotosIndex";
+
 function Home() {
+  const [photos, setPhotos] = useState([]);
+  useEffect(function () {
+    axios.get("http://localhost:3000/photos.json").then((response) => {
+      console.log("photos index", response.data);
+      setPhotos(response.data);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <PhotosIndex photos={photos} />
     </div>
   );
 }
